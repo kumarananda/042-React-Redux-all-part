4781575dda1a2279455571cae77ede9952181805
kumarananda <coderanandakumar@gmail.com>
2022-09-27 09:08:54 +0600

    redux middleware permission

--- 042.02 Redux with React Redux part 02/src/App.js
+++ 042.02 Redux with React Redux part 02/src/App.js
@@ -3,18 +3,31 @@ import React,{ useState } from 'react';
 import { useSelector } from 'react-redux';
 import Devs from './pages/Devs/Devs';
 
+import Colors from './components/colors/Colors';
+import Counter from './components/counter/Counter';
+import Foods from './components/foods/Foods';
+import SingleFood from './components/foods/SingleFood';
 
 
 
 
 function App() {
 
-
+  const {color} = useSelector(state => state)
 
 
   return (
     <>
+      
       <Devs/>
+
+      <div style={{background : color}} className="main-container">
+        <Counter/>
+        <Colors/>
+        <Foods/>
+        <SingleFood/>
+      </div>
+
     </>
   );
 }
--- 042.02 Redux with React Redux part 02/src/redux/store.js
+++ 042.02 Redux with React Redux part 02/src/redux/store.js
@@ -1,10 +1,11 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import rootReducer from './rootReducer';
 import {composeWithDevTools } from 'redux-devtools-extension'
+import checkMiddleware from '../middleware/checkMiddleware';
 
 
 // create redux store
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(checkMiddleware)));
 
 // export redux store
 export default store
