0654e6eac22ec90619c67d926480ae1119b1fa60
kumarananda <coderanandakumar@gmail.com>
2022-09-28 18:14:01 +0600

    Add startup MERN project as 42.03

---
+++ 042.03 Redux with Startup Project/api/middlewares/userMeddleware.js
@@ -0,0 +1,49 @@
+import createError from "../controllers/createError.js";
+import jwt from 'jsonwebtoken';
+
+
+// check user authenticated or not
+const userMeddleware = (req, res, next) => {
+
+    try{
+
+        const token = req.cookies.access_token;
+    
+        // check token 
+        if(!token){
+            return next(createError(401, "You are not authenticated"))
+        }
+
+        // check token authenticated or not
+        const tokenCheck = jwt.verify(token, process.env.JWT_SECRET);
+
+        // if token  not authenticated
+        if(!tokenCheck){
+            return next(createError(401, "Token is Expired"))
+        }
+
+        // // compare user data
+        if(tokenCheck.id !== req.params.id ){
+            return next(createError(401, "You are not able to access this data"))
+        }
+
+        // console.log(tokenCheck);
+        // if token  is authenticated
+        if(tokenCheck){
+            req.user =  tokenCheck;
+            next()
+        }
+        
+
+        // next();
+
+    }catch(error){
+        next(error);
+    }
+
+    
+
+    
+}
+
+export default userMeddleware;
---
+++ 042.03 Redux with Startup Project/api/server.js
@@ -0,0 +1,39 @@
+// import { Express } from "express";
+import  express  from "express";
+import colors from "colors";
+import dotenv from 'dotenv';
+import studentRoutes from './routes/studentsRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import mongoDBConnect from "./config/db.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import cookieParser from "cookie-parser";
+
+// express init
+const app = express()
+dotenv.config()
+
+
+// middlewares // body parser
+app.use(express.json());
+app.use(express.urlencoded({extended : false}))
+app.use(cookieParser());
+
+
+// env variabels
+const PORT = process.env.SERVER_PORT || 5000
+
+
+
+// routes
+app.use('/api/students', studentRoutes);
+app.use('/api/user', userRoutes);
+
+// error handler
+app.use(errorHandler)
+
+// listen server
+app.listen(PORT, () => {
+    // mongoDB init
+    mongoDBConnect()
+    console.log(`server running on port ${PORT}` .bgGreen.black);
+})
